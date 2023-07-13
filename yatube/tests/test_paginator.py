7308e62aa904251af4ae117f5119b2d0634be04a
Александр <nevolin_alexandrr@mail.ru>
2023-07-13 17:50:38 +0300

    Create test_profile

--- yatube/tests/test_paginator.py
+++ yatube/tests/test_paginator.py
@@ -3,13 +3,13 @@ import pytest
 from django.core.paginator import Paginator, Page
 
 
-class TestGroupView:
+class TestGroupPaginatorView:
 
 	@pytest.mark.django_db(transaction=True)
 	def test_group_paginator_view_get(self, client, post_with_group):
 		try:
 			response = client.get(f'/group/{post_with_group.group.slug}')
-		except Exception:
+		except Exception as e:
 			assert False, f'''неправильно работает страница /group/slug, ошибка {e}'''
 		if response.status_code in (301, 302):
 			response = client.get(f'/group/{post_with_group.group.slug}')
@@ -23,4 +23,23 @@ class TestGroupView:
 		assert 'page' in response.context, \
 			f'''Проверьте, что передали переменную `page` в контекст страницы `/group/<slug>/`'''
 		assert type(response.context['page']) == Page, \
-			f'''Проверьте, что переменная `page` является типом `page`''' 
+			f'''Проверьте, что переменная `page` является типом `page`'''
+
+	@pytest.mark.django_db(transaction=True)
+	def test_index_paginator_view_get(self, client, post_with_group):
+		try:
+			response = client.get(f'/')
+		except Exception:
+			assert False, f'''неправильно работает начальная страница `/`, ошибка {e}'''
+		assert response.status_code != 404, \
+			f'''Стартовая страница `/` не найдена, проверьте этот адрес в *urls.py*'''
+
+		assert 'paginator' in response.context, \
+			f'''Проверьте, что передали переменную `paginator` в контекст страницы `/group/<slug>/`'''
+		assert type(response.context['paginator']) == Paginator, \
+			f'''Проверьте, что переменная `paginator` является типом `paginator`''' 
+		assert 'page' in response.context, \
+			f'''Проверьте, что передали переменную `page` в контекст страницы `/group/<slug>/`'''
+		assert type(response.context['page']) == Page, \
+			f'''Проверьте, что переменная `page` является типом `page`'''
+
